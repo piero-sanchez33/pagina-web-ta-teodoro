ae1b176b450200908becc38447267657cd713541
piero-sanchez33 <pieritocrack2@gmail.com>
2026-05-07 22:23:56 -0500

    Use environment variable for server port

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ app.post("/registro", async (req, res) => {
 });
 
 // SERVER
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor corriendo");
 });
